e23ab0a2c0634e3ccec32756deb6e068e9cd62d2
German <german@pushkin-spb.com>
2023-02-19 19:51:22 +0300

    first commit

---
+++ calculator/tests.py
@@ -0,0 +1,11 @@
+from django.test import TestCase
+
+# Create your tests here.
+# DATA = {
+#     'omlet': {
+#         'яйца, шт': 2,
+#         'молоко, л': 0.1,
+#         'соль, ч.л.': 0.5
+#     }
+# }
+# a = DATA['omlet']
---
+++ calculator/views.py
@@ -0,0 +1,76 @@
+from django.shortcuts import render
+
+DATA = {
+    'omlet': {
+        'яйца, шт': 2,
+        'молоко, л': 0.1,
+        'соль, ч.л.': 0.5,
+    },
+    'pasta': {
+        'макароны, г': 0.3,
+        'сыр, г': 0.05,
+    },
+    'buter': {
+        'хлеб, ломтик': 1,
+        'колбаса, ломтик': 1,
+        'сыр, ломтик': 1,
+        'помидор, ломтик': 1,
+    },
+    # можете добавить свои рецепты ;)
+}
+
+
+def omlet(request):
+    servings = request.GET.get("servings")
+    if servings == None:
+        context = {
+            'recipe': DATA['omlet']
+        }
+    else:
+        dic = {}
+        for key, value in DATA['omlet'].items():
+            dic[key] = value * int(servings)
+        context = {
+                'recipe': dic
+            }
+    return render(request, 'calculator/index.html', context)
+
+def pasta(request):
+    servings = request.GET.get("servings")
+    if servings == None:
+        context = {
+            'recipe': DATA['pasta']
+        }
+    else:
+        dic = {}
+        for key, value in DATA['pasta'].items():
+            dic[key] = value * int(servings)
+        context = {
+            'recipe': dic
+        }
+    return render(request, 'calculator/index.html', context)
+
+def buter(request):
+    servings = request.GET.get("servings")
+    if servings == None:
+        context = {
+            'recipe': DATA['buter']
+        }
+    else:
+        dic = {}
+        for key, value in DATA['buter'].items():
+            dic[key] = value * int(servings)
+        context = {
+            'recipe': dic
+        }
+    return render(request, 'calculator/index.html', context)
+
+# Напишите ваш обработчик. Используйте DATA как источник данных
+# Результат - render(request, 'calculator/index.html', context)
+# В качестве контекста должен быть передан словарь с рецептом:
+# context = {
+#   'recipe': {
+#     'ингредиент1': количество1,
+#     'ингредиент2': количество2,
+#   }
+# }
